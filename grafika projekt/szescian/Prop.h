3d1e1ae281188541d1cf0bd49b6d687dbc2e5294
lpast48 <ukaszpasternak@gmail.com>
2017-04-01 12:49:43 +0200

    lib deleted

--- grafika projekt/szescian/Prop.h
+++ grafika projekt/szescian/Prop.h
@@ -1,31 +1,4 @@
 #pragma once
-
-#define PI 3.14159265
-
-#ifdef _MSC_VER                         // Check if MS Visual C compiler
-#  pragma comment(lib, "opengl32.lib")  // Compiler-specific directive to avoid manually configuration
-#  pragma comment(lib, "glu32.lib")     // Link libraries
-#endif
-
-// Ustalanie trybu tekstowego:
-#ifdef _MSC_VER        // Check if MS Visual C compiler
-#   ifndef _MBCS
-#      define _MBCS    // Uses Multi-byte character set
-#   endif
-#   ifdef _UNICODE     // Not using Unicode character set
-#      undef _UNICODE 
-#   endif
-#   ifdef UNICODE
-#      undef UNICODE 
-#   endif
-#endif
-
-#include <windows.h>            // Window defines
-#include <gl\gl.h>              // OpenGL
-#include <gl\glu.h>             // GLU library
-#include <math.h>				// Define for sqrt
-#include <stdio.h>
-
 #include "Basic3D.h"
 
 class Prop :
